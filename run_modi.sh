5eca4d56254038121fec0d1cae4cdc01ec342520
Juhyun Kim <juhyun@corelab.or.kr>
2015-07-09 16:24:32 +0900

    experiment

--- run_modi.sh
+++ run_modi.sh
@@ -7,3 +7,6 @@ g++ -fPIC -Wl,-no-undefined  -m64 -o filter_Acts_XxY_sparse_modi.exe filter_Acts
 ./filter_Acts_XxY_sparse_modi.exe > result.txt
 diff result.txt zero-out_target.data 
 #rm result.txt
+
+
+#
