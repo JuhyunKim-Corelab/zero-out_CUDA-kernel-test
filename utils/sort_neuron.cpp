9a4bc7e8b76c64850d81f7f92c4c3c47e32c2e0e
Juhyun Kim <juhyun@corelab.or.kr>
2015-07-17 11:55:28 +0900

    nLoadForBlocks.data will contain #of load for first thread!

--- utils/sort_neuron.cpp
+++ utils/sort_neuron.cpp
@@ -6,6 +6,7 @@
 #include <vector>       // std::vector
 #include <string>
 #define MAX_BUF 65536
+#define BLOCKSIZE 32
 
 //stable sort in ascending orders
 //g++ sort_neuron.cpp -o sort_neuron.exe
@@ -24,6 +25,7 @@ int main(){
 
     std::vector<Neuron> nvec;
     FILE *fp;
+    FILE *fp_lfb;
     FILE *fp_target;
     FILE *fp_mapping;
     int ret, newline;
@@ -32,6 +34,7 @@ int main(){
     unsigned int nzcnt = 0;
     float tmp;
     char filename_sort[40] = "nzConnSort.data";
+    char filename_lfb[40] = "nLoadForBlocks.data";
     char filename_target[40] = "nzConnUnsort.data";//produced by target.data
     char filename_mapping[40] = "nzConnMapping.data";
 
@@ -45,6 +48,10 @@ int main(){
     }
     if((fp = fopen(filename_sort, "w")) == NULL) {
         printf("No such file\n");
+        exit(1);
+    }
+    if((fp_lfb = fopen(filename_lfb, "w")) == NULL) {
+        printf("No such file(nLoadForBlocks)\n");
         exit(1);
     }
 
@@ -80,6 +87,7 @@ int main(){
     //    printf("%d, %d\n", nvec[i].nNonzeroConn, nvec[i].idx_original);
     //}
 
+    unsigned threadCnt = 0; // will iteration across 0~(BLOCKSIZE-1) again and again
     std::stable_sort (nvec.begin(), nvec.end(), compare_nzConn);
     //printf("after sort, size: %ld\n", nvec.size()); 
     for (int i = 0; i < nvec.size(); ++i){
@@ -89,12 +97,16 @@ int main(){
 
             //print it to a separate files
             fprintf(fp, "%d\n", nvec[i].nNonzeroConn);
+            if(threadCnt == 0)//number of load in first thread
+                fprintf(fp_lfb, "%d\n", nvec[i].nNonzeroConn);
             fprintf(fp_mapping, "%d\n", nvec[i].idx_original);
             nzcnt++;
+            threadCnt = (threadCnt+1)%BLOCKSIZE;
         } 
     }
     printf("number of living Neuron: %d, %d/32 = %d, %d mod 32 = %d \n", nzcnt, nzcnt, nzcnt/32, nzcnt,nzcnt%32);
     fclose(fp);
+    fclose(fp_lfb);
     fclose(fp_target);
     fclose(fp_mapping);
     return 0;
